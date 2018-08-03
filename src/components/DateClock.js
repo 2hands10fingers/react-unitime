a3689f65d9e189fd549bce57c08cccf0fbb89538
Antonio Nogueras <antonio@square205.com>
2018-08-02 23:25:11 -0500

    added some URL parsing logic

--- src/components/DateClock.js
+++ src/components/DateClock.js
@@ -13,30 +13,81 @@ class DateClock extends React.Component {
         }
     }
 
+
+    timeArgChecker = timeEntered => {
+        let urlArgs = window.location.search.slice(1)
+                        .split('&')
+                        .reduce(function _reduce (/*Object*/ a, /*String*/ b) {
+                        b = b.split('=');
+                        a[b[0]] = decodeURIComponent(b[1]);
+                        return a;
+                        }, {});
+
+        if (timeEntered < 0) alert("WTF MAN!")
+        
+
+        if (timeEntered.length === 4) {
+            
+            this.setState( 
+                {
+                    initHours:   timeEntered.substring(0, 2),
+                    initMinutes: timeEntered.substring(2,4)
+                }
+            )
+
+            console.log(this.state)
+        }
+
+        if (timeEntered === 3) {
+            this.setState({
+                initHours: "0" + timeEntered.substring(0,1),
+                initMinutes: timeEntered.substring(1,2)
+            })
+        }
+    }
+
+
     componentDidMount() {
+        let urlArgs = window.location.search.slice(1)
+                            .split('&')
+                            .reduce(function _reduce (/*Object*/ a, /*String*/ b) {
+                            b = b.split('=');
+                            a[b[0]] = decodeURIComponent(b[1]);
+                            return a;
+                            }, {});
+        
         function zeroAdd(time) {
             if (time < 10) return "0" + time
         }
 
+
+
+        // console.log(urlArgs[""])
         let currentTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
         let dateObject = new Date()
         let currentTimeHours = dateObject.getHours();
         let currentTimeMinutes = dateObject.getMinutes();
         let stringMinutes = zeroAdd(currentTimeMinutes)
 
-        if (this.state.initTime === undefined) {
+        if (urlArgs[""] === 'undefined') {
             console.log(currentTimeHours, ':', ',', currentTimeMinutes);
             
             this.setState(
-                { initHours: currentTimeHours,
+                { 
+                  initHours: currentTimeHours,
                   initMinutes: currentTimeMinutes,
                   initTimezone: currentTimeZone,
                   initTime: `${currentTimeHours}:${currentTimeMinutes}, ${currentTimeZone}`,
                   stringTimes: {
-                      initMinutes: stringMinutes
-                  }
+                      initMinutes: stringMinutes}
                 }
             )
+            
+        } else {
+            console.log('sup')
+            Object.keys(urlArgs).forEach( i => console.log(urlArgs[i]))
+            this.timeArgChecker(urlArgs["t"])
+            console.log(this.state)
         }
 
     }
